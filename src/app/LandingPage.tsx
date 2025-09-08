60207acfb05ccf89413fab53759db0c3c92a8786
jasleen.singh <jasleen.singh@antiersolutions.com>
2025-09-08 13:46:09 +0530

    start project

---
+++ src/app/LandingPage.tsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Car Dealership</h1>
+      <p>Find your dream car today!</p>
+    </div>
+  );
+}
+
+export default LandingPage;
